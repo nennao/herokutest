4b4fac10abfbb98fd1ce00cdc68f560aef1ca7aa
Nenna <ninaokoye@outlook.com>
2019-07-08 15:43:12 +0100

    requests testing

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import random
+import requests
+from flask import Flask, jsonify, request
 from flask import render_template
 
 app = Flask(__name__)
@@ -12,6 +14,29 @@ def home():
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+
+@app.route('/data/coin')
+def get_coin_data():
+    params = {'start': '2017-12-31',
+              'end': '2018-01-15',
+              }
+    req = requests.get(url="https://api.coindesk.com/v1/bpi/historical/close.json", params=params)
+    data = req.json()
+
+    return data['bpi']
+
+
+@app.route('/data/random', methods=["GET"])
+def get_rand_data():
+    return jsonify({'random': round(2+random.random()*3, 3)})
+
+
+@app.route('/data/post', methods=["POST"])
+def post_test():
+    name = request.args["name"]
+    test_str = f'congratulations, {name} on your successful request'
+    return jsonify(f"post request for '{name}' was successful. {test_str}"), 200
 
 
 if __name__ == '__main__':
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,28 @@
 $( "#button" ).click(function() {
   alert( "nice" );
 });
+
+const rawData = {"2017-12-31":13860.1363,"2018-01-01":13412.44,"2018-01-02":14740.7563,"2018-01-03":15134.6513,"2018-01-04":15155.2263,"2018-01-05":16937.1738,"2018-01-06":17135.8363,"2018-01-07":16178.495,"2018-01-08":14970.3575,"2018-01-09":14439.4738,"2018-01-10":14890.7225,"2018-01-11":13287.26,"2018-01-12":13812.715,"2018-01-13":14188.785,"2018-01-14":13619.0288,"2018-01-15":13585.9013};
+
+
+function parseData(data) {
+  let arr = [];
+  for (let i in Object.keys(data)) {
+    arr.push({
+      date: new Date(i),
+      value: +data[i]
+    });
+  }
+  return arr
+}
+
+let data = parseData(rawData);
+
+function drawChart(data) {
+  const svgWidth = 600, svgHeight = 400;
+  const margin = {top: 20, right: 20, bottom: 30, left: 50};
+  const width = svgWidth - margin.left - margin.right;
+  const height = svgHeight - margin.top - margin.bottom;
+
+  const svg = d3.select('#testD3').attr("width", svgWidth).attr("height", svgHeight);
+}
