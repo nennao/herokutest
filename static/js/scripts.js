03bc8f4502bdc2b94e9b36944f3f42a9b7d30200
Nenna <ninaokoye@outlook.com>
2019-07-09 08:00:32 +0100

    cors test

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,23 @@
 $( "#button" ).click(function() {
-  alert( "nice" );
+  // alert( "nice" );
+  fetch('https://safe-crag-49936.herokuapp.com/data/random')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson['random']));
+  });
+});
+
+$( "#button2" ).click(function() {
+  // alert( "nice" );
+  fetch('https://safe-crag-49936.herokuapp.com/data/post?name=nemes', {method: 'POST'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
 });
 
 const rawData = {"2017-12-31":13860.1363,"2018-01-01":13412.44,"2018-01-02":14740.7563,"2018-01-03":15134.6513,"2018-01-04":15155.2263,"2018-01-05":16937.1738,"2018-01-06":17135.8363,"2018-01-07":16178.495,"2018-01-08":14970.3575,"2018-01-09":14439.4738,"2018-01-10":14890.7225,"2018-01-11":13287.26,"2018-01-12":13812.715,"2018-01-13":14188.785,"2018-01-14":13619.0288,"2018-01-15":13585.9013};
